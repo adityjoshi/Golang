f9f1c2a6f87af9218ac73edd092b90dd4e3cef5a
Aditya Joshi <aditya03joshi@gmail.com>
2024-02-01 23:06:48 +0530

    go

---
+++ 2-PrintFormat/main.go
@@ -0,0 +1,21 @@
+package main 
+
+import "fmt"
+
+func main() {
+	age := 19
+	name := "Adi"
+	fmt.Println("hello")
+	fmt.Println("hey i am ", name, " my age is ", age )
+	
+	// formated strings 
+	// %v common for variable
+	fmt.Printf("hey i am %v and my age is %v \n " , name, age )
+	fmt.Printf("hey i am %q and my age is %v \n", name , age)
+	// %q puts quotes over the string only 
+
+	// Sprintf saves the formatted strings 
+	var str = fmt.Sprintf("hey i am %v and my age is %v \n", name, age)
+	fmt.Print(str)
+
+}
---
+++ dataType/main.go
@@ -0,0 +1,19 @@
+package main 
+
+import "fmt"
+
+func main() {
+	// strings 
+	var name string = "adi"
+	var secname = "muk"
+	fmt.Println(name)
+	fmt.Println(secname)
+
+	naam := "adi"
+	print(naam)
+
+	// bits & memories 
+	var num int8 = 127
+	fmt.Print(num)
+
+}
