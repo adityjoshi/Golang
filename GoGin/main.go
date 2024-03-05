3fe3c5136caa54d6ad1551cf9ace4ee254fa8a07
Aditya Joshi <aditya03joshi@gmail.com>
2024-03-05 22:32:37 +0530

    timeforgo

--- GoGin/main.go
+++ GoGin/main.go
@@ -7,12 +7,34 @@ import (
 )
 
 func main() {
-	r := gin.Default()
+	// this is a handler fucntion
+	r := gin.New()
+	r.Use(gin.Logger())
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 			"hey":     "they",
 		})
 	})
+	r.GET("/getdata", getdata)
+	r.POST("/getdatapost", getpost)
+	r.GET("/query",getquery)
 	r.Run()
+}
+
+func getdata(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"hey": "i am a function",
+	})
+
+}
+func getpost(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"hey": "post is working fine",
+	})
+func getquery(c *gin.Context) {
+	
+}
+
+
 }
